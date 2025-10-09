e1e1633fd54cec7cd5fe7cb02f33956433eeabb9
Aleksa Stankovic <aleksa.stankovic@htecgroup.com>
2025-10-09 17:11:59 +0200

    Positive tests

--- src/test/java/test/suites/UsersTests.java
+++ src/test/java/test/suites/UsersTests.java
@@ -1,8 +1,12 @@
 package test.suites;
 
+import DataModels.BooksRequest;
+import DataModels.BooksResponse;
 import DataModels.UsersRequest;
 import DataModels.UsersResponse;
+import DataProvider.BooksProvider;
 import DataProvider.UsersProvider;
+import calls.BooksApi;
 import calls.UsersApi;
 //import jdk.jfr.Description;
 import org.testng.Assert;
@@ -12,8 +16,6 @@ import test.asserts.UsersAsserts;
 import test.common.TestBase;
 
 public class UsersTests extends TestBase {
-
-
 
     public static void main(String[] args) {
 
@@ -31,7 +33,26 @@ public class UsersTests extends TestBase {
         idOfNewUser = UsersApi.postUser(usersRequest).getId();
     }
 
+    @Test
+    //@Description("Verify user has been created")
+    public void createUserTest(){
 
+        UsersResponse createUserResponse = UsersApi.postUser(usersRequest);
 
+        usersAsserts.assertCreateNewUser(usersRequest, createUserResponse);
+    }
+
+    @Test
+    //@Description("Verify that user can be updated")
+    public void updateBookTest() {
+
+        UsersRequest updatedUserRequest = UsersProvider.prepareUsersRequest();
+
+        updatedUserRequest.setId(idOfNewUser);
+
+        UsersResponse updatedUserResponse = UsersApi.updateUser(updatedUserRequest);
+
+        usersAsserts.assertCreateNewUser(updatedUserRequest, updatedUserResponse);
+    }
 }
 
