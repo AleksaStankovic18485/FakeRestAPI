ac1e7d52f73c4da4cc924777185394e9dcf97e25
Aleksa Stankovic <aleksa.stankovic@htecgroup.com>
2025-10-09 12:48:45 +0200

    final prep

--- src/test/java/DataProvider/BooksProvider.java
+++ src/test/java/DataProvider/BooksProvider.java
@@ -1,4 +1,36 @@
 package DataProvider;
 
+import DataModels.BooksRequest;
+import com.github.javafaker.Faker;
+import common.ValueChoosers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class BooksProvider {
+
+    public static BooksRequest prepareBooksRequest() {
+        BooksRequest booksRequest = new BooksRequest();
+        booksRequest.setId(Faker.instance().random().nextInt(105));
+        booksRequest.setTitle(ValueChoosers.getRandomTitle());
+        booksRequest.setDescription(Faker.instance().lorem().sentence());
+        booksRequest.setPageCount(Faker.instance().number().numberBetween(60, 100));
+        booksRequest.setExcerpt(Faker.instance().leagueOfLegends().quote());
+
+
+
+
+
+
+        SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        isoFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date randomDate = Faker.instance().date().birthday();
+        String formattedDate = isoFormat.format(randomDate);
+
+        booksRequest.setPublishDate(formattedDate);
+
+        return  booksRequest;
+
+    }
 }
--- src/test/java/suites/FakeRestTest.java
+++ src/test/java/suites/FakeRestTest.java
@@ -1,34 +1,34 @@
-package suites;
-
-import DataModels.Books;
-import calls.BooksApi;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import io.restassured.response.Response;
-import org.testng.asserts.SoftAssert;
-
-import java.lang.reflect.Type;
-import java.util.List;
-
-public class FakeRestTest {
-
-    public SoftAssert softAssert= new SoftAssert();
-
-    public static boolean isBooksExists(Integer id) {
-
-        Response response = BooksApi.listOfBooksResponse();
-        String json = response.getBody().asString();
-
-        Type listType = new TypeToken<List<Books>>() {}.getType();
-        List<Books> booksList = new Gson().fromJson(json, listType);
-
-        for (Books book : booksList) {
-            if (book.getId().equals(id)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-}
-
+//package suites;
+//
+//import DataModels.Books;
+//import calls.BooksApi;
+//import com.google.gson.Gson;
+//import com.google.gson.reflect.TypeToken;
+//import io.restassured.response.Response;
+//import org.testng.asserts.SoftAssert;
+//
+//import java.lang.reflect.Type;
+//import java.util.List;
+//
+//public class FakeRestTest {
+//
+//    public SoftAssert softAssert= new SoftAssert();
+//
+//    public static boolean isBooksExists(Integer id) {
+//
+//        Response response = BooksApi.listOfBooksResponse();
+//        String json = response.getBody().asString();
+//
+//        Type listType = new TypeToken<List<Books>>() {}.getType();
+//        List<Books> booksList = new Gson().fromJson(json, listType);
+//
+//        for (Books book : booksList) {
+//            if (book.getId().equals(id)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//
+//}
+//
